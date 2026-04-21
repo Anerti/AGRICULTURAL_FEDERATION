be1222d1aefd4092d8e95b394a4c69d3fc5bc30e
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-04-21 15:14:55 +0300

    initial commit

---
+++ src/main/java/com/example/agricultural_federation/AgriculturalFederationApplication.java
@@ -0,0 +1,13 @@
+package com.example.agricultural_federation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalFederationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriculturalFederationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agricultural_federation/AgriculturalFederationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agricultural_federation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalFederationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
